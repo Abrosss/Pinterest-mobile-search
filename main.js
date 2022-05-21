1e7a24faa2adb3c27640d2cc4f489eabf8ac8966
Abrosss <nataroro5@gmail.com>
2022-05-21 10:29:11 +0600

    added buttons functionality

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
+const input = document.querySelector('.search')
+const cameraButton = document.querySelector('.camera')
+const clearButton = document.querySelector('.clearAll')
+console.log(clearButton) 
 
+
+function clear(){
+    cameraButton.classList.add('hide')
+    clearButton.classList.add('show')
+    if (input.value.length === 0) {
+        cameraButton.classList.remove('hide')
+        clearButton.classList.remove('show')
+      }
+}
+
+
+
+input.addEventListener('change', clear)
+input.addEventListener('keyup', clear);
